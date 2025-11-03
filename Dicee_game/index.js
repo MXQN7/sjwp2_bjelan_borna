df5cac2f8c7f0b3f3e4f9e3f77f72247d943fc3e
MXQN7 <borna.bjelan@gmail.com>
2025-11-03 17:22:23 +0100

    javascript

---
+++ Dicee_game/index.js
@@ -0,0 +1,21 @@
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+console.log(randomNumber1);
+let randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+console.log(randomNumber2);
+let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🏆Player 1 Wins";
+}
+
+else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins🏆";
+}
+
+if(randomNumber1 === randomNumber2){
+    document.querySelector("h1").innerHTML = "Draw";
+}
